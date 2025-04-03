68ec62b967864d214f28185c2aeb095b14d28840
mukular <mukulgarg6194@gmail.com>
2025-04-04 04:17:41 +0530

    Code till video 7 is written

---
+++ Folder7/File7chai.js
@@ -0,0 +1,34 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*5);
+console.log(2**5);
+console.log(2/3);
+console.log(22%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// Don't do like above two lines , use brackets instead
+
+console.log(+true);
+console.log(+""); 
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
