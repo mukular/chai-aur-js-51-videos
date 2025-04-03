1c0d051b2499f7db84159070be6891ef31063f61
mukular <mukulgarg6194@gmail.com>
2025-04-04 03:48:28 +0530

    Code till video 5 is written

---
+++ Folder5/File5chai.js
@@ -0,0 +1,34 @@
+// Data types
+
+/*
+pahle ki js aur ab ki js me bahut difference aa gaya h 
+*/
+
+"use strict";    // treat all JS code as newer version  // bina iske bhi kaam ho jayega
+
+// alert(3 + 3)  We are using nodeJs, not browser so can't use this here
+
+console.log(3 + 3);
+
+console.log("Hitesh");
+
+let name = "hitesh"   // string
+let age = 18  // number
+let isLoggedIn = false  // boolean
+let state;  // undefined
+let temperature = null
+
+// number Range  =>  2 to power 53
+// bigint
+// string  => ""
+// boolean => true or false
+// null => standalone value
+// undefined
+// symbol => unique
+// object
+
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
