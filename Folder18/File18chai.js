ce04b58cd8cdcdc796fc38540c9d481a1da109a7
mukular <mukulgarg6194@gmail.com>
2025-04-16 08:39:50 +0530

    Code till video 18 is written

---
+++ Folder18/File18chai.js
@@ -0,0 +1,11 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+const {courseInstructor} = course;
+console.log(courseInstructor);
+
+const {coursename: cname} = course;
+console.log(cname);
